6e136261f933bd7c9e6443e00285639e5fe4bc9c
Amy Drelicharz <adrelicharz1097@jh-mba158-07.d214.org>
2018-03-07 11:19:41 -0600

    added AnimalType

--- PersonalityQuiz/questionData.swift
+++ PersonalityQuiz/questionData.swift
@@ -20,5 +20,21 @@ enum ResponseType {
 
 struct Answer {
     var text : String
-    var type: AnimalType
+    var type : AnimalType
+}
+
+enum AnimalType : Character {
+    case dog = "🐶", rabbit = "🐰", turtle = "🐢", cat = "🐱"
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surrond yourself with people you love, and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy dong things on your own terms."
+        case .rabbit:
+            return "You love everthing that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
 }
