4deb7471c674edecfb6aa979c7971a5e708ae0c4
Amy Drelicharz <adrelicharz1097@jh-mba158-07.d214.org>
2018-03-12 11:25:11 -0500

    added switch statement to update UI

--- PersonalityQuiz/QuestionsViewController.swift
+++ PersonalityQuiz/QuestionsViewController.swift
@@ -68,8 +68,8 @@ class QuestionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        // Do any additional setup after loading the view.
+            updateUI()
+        
     }
 
     func updateUI() {
@@ -85,6 +85,15 @@ class QuestionsViewController: UIViewController {
         navigationItem.title = "Question #\(questionIndex + 1)"
         questionLabel.text = currentQuestion.text
         progressView.setProgress(totalProgress, animated: true)
+        
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultipleStack(using: currentAnswers)
+        case .ranged:
+            updateRangedStack(using: currentAnswers)
+        }
     }
     
     func updateSingleStack (using answers: [Answer]) {
@@ -111,18 +120,7 @@ class QuestionsViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
